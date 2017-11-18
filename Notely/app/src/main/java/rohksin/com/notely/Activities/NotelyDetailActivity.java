bcd798b330b44775e25830307c2a88d05b18066f
rohitksingh <r4rohit002@gmail.com>
2017-11-17 23:40:23 -0800

    DetailActivity added with coordinator layout

--- Notely/app/src/main/java/rohksin/com/notely/Activities/NotelyDetailActivity.java
+++ Notely/app/src/main/java/rohksin/com/notely/Activities/NotelyDetailActivity.java
@@ -1,8 +1,16 @@
 package rohksin.com.notely.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import rohksin.com.notely.Adapters.NotelyListAdapter;
 import rohksin.com.notely.Models.Note;
@@ -14,14 +22,60 @@ import rohksin.com.notely.R;
 
 public class NotelyDetailActivity extends AppCompatActivity {
 
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private Toolbar toolbar;
+    private TextView gist;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notely_detail_actiivty);
 
-        Note note = (Note)getIntent().getSerializableExtra(NotelyListAdapter.LIST_NOTE);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        gist = (TextView)findViewById(R.id.mainGist);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.title);
+        Note note = (Note)getIntent().getSerializableExtra(NotelyListAdapter.LIST_NOTE);
+        collapsingToolbarLayout.setTitle(note.getTitle());
+        gist.setText(note.getGist()+",mzx,mz,mx,zm,xm,zm");
         Log.d("NOTE", note.toString());
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.notely_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        int id = item.getItemId();
+
+
+        if(id == android.R.id.home)
+        {
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
 }
